0378844508eb0c7ea64aa9a1f1c4a5fd0b5a4897
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-07-02 12:43:26 +0200

    run a search

---
+++ README.md
@@ -0,0 +1,25 @@
+# Retail Search MIG
+
+## Setup
+
+1.
+
+```sh
+npm i
+```
+
+2.
+
+```sh
+touch .env
+```
+
+Inside of `.env` file, add the following:
+
+```
+PROJECT_NUMBER=[PROJECT_NUMBER]
+SERVICE_ACCOUNT_PATH=[SERVICE_ACCOUNT_PATH]
+```
+
+Service account path is the path to a service account with `retail.editor`
+permissions
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,67 @@
-// src/index.ts
-const message: string = "hello world";
-console.log(message);
+import "dotenv/config";
+import {
+  SearchServiceClient,
+  CompletionServiceClient,
+  protos,
+} from "@google-cloud/retail";
+
+const mustGetEnv = (key: string) => {
+  if (!process.env[key]) {
+    throw new Error(`Missing environment variable ${key}`);
+  }
+  return process.env[key];
+};
+
+const PROJECT_NUMBER = mustGetEnv("PROJECT_NUMBER");
+const SERVICE_ACCOUNT_PATH = mustGetEnv("SERVICE_ACCOUNT_PATH");
+const CATALOG = `projects/${PROJECT_NUMBER}/locations/global/catalogs/default_catalog`;
+const PLACEMENT = `projects/${PROJECT_NUMBER}/locations/global/catalogs/default_catalog/placements/default_search`;
+const BRANCH = `projects/${PROJECT_NUMBER}/locations/global/catalogs/default_catalog/branches/0`;
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = SERVICE_ACCOUNT_PATH;
+
+console.log("PROJECT_NUMBER:", PROJECT_NUMBER);
+console.log("SERVICE_ACCOUNT_PATH:", SERVICE_ACCOUNT_PATH);
+
+class RetailSearchClient {
+  private searchServiceClient: SearchServiceClient;
+  private completionServiceClient: CompletionServiceClient;
+
+  constructor() {
+    this.searchServiceClient = new SearchServiceClient();
+    this.completionServiceClient = new CompletionServiceClient();
+  }
+
+  async search(query: string, visitorId: string) {
+    const req = {
+      query,
+      visitorId,
+      placement: PLACEMENT,
+      branch: BRANCH,
+      pageSize: 20,
+    };
+    console.log(req);
+    const res = await this.searchServiceClient.search(req);
+    return res;
+  }
+
+  async autocomplete(query: string, visitorId: string) {
+    const req = {
+      query,
+      visitorId,
+      dataset: "cloud-retail",
+      catalog: CATALOG,
+    };
+    console.log(req);
+    const res = await this.completionServiceClient.completeQuery(req);
+    return res;
+  }
+}
+
+const main = async () => {
+  const client = new RetailSearchClient();
+  const res = await client.search("hello", "visitor1");
+  console.log(res);
+};
+
+main();
