9645e476203a1badf58f8e853477c289d23c3670
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-09-02 20:01:00 +0200

    move mustgetenv

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,10 @@
+export const mustGetEnv = (key: string) => {
+  if (!process.env[key]) {
+    throw new Error(`Missing environment variable ${key}`);
+  }
+  return process.env[key];
+};
+
 export const PROJECT_NUMBER = mustGetEnv("PROJECT_NUMBER");
 export const SERVICE_ACCOUNT_PATH = mustGetEnv("SERVICE_ACCOUNT_PATH");
 export const CATALOG = `projects/${PROJECT_NUMBER}/locations/global/catalogs/default_catalog`;
